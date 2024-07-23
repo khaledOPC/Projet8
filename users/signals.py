af4762869205ad69d38e23b46b07491b34d4fa3a
Khaled Djema <khaled.OPC92@gmail.com>
2024-07-23 22:53:36 +0200

    ajout d'un fichier signals.py

---
+++ users/signals.py
@@ -0,0 +1,12 @@
+# users/signals.py
+
+# Ajoutez vos signaux ici, par exemple :
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.contrib.auth.models import User
+
+# @receiver(post_save, sender=User)
+# def my_signal_handler(sender, instance, created, **kwargs):
+#     if created:
+#         # Action à réaliser lors de la création d'un utilisateur
+#         pass
